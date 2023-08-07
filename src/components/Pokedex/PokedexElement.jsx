3e3caa9a85fe2794036003536bbd3d839ef7c8be
Polamento <xavierdobato@gmail.com>
2023-08-07 19:15:30 +0200

    afegit setImages a la vista de pochemon

--- src/components/Pokedex/PokedexElement.jsx
+++ src/components/Pokedex/PokedexElement.jsx
@@ -9,9 +9,9 @@ function PokedexElement(pokemon) {
             try {
 
                 const response = await getPokemonInfo(pokemon.pokemon.name)
-                setPokemonInfo(response.data.results);
+                setPokemonInfo(response.data);
 
-                console.log(setPokemonInfo)
+
             }
 
             catch (e) {
@@ -22,9 +22,12 @@ function PokedexElement(pokemon) {
         fetchPokemons();
     }, []);
 
+    // ?. comprova si existeix
+
     return (
         <div className='flex flex-col items-center justify-center p-3 m-2 bg-slate-400 text-white capitalize rounded-lg'>
             <h1>{pokemon.pokemon.name}</h1>
+            <img src={pokemonInfo.sprites?.front_default} alt="" /> 
         </div>
 
     )
