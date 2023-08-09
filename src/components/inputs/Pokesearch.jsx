3f00b3db185cbf2b44ebd0690e29c69fe66e45d4
Polamento <98086877+xaviercaju@users.noreply.github.com>
2023-08-09 22:44:23 +0200

    searchbar init

--- src/components/inputs/Pokesearch.jsx
+++ src/components/inputs/Pokesearch.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 import { getAllPokemons } from "../../api/api";
 
 function Pokesearch() {
-  const [pokemons, setPokemons] = useState([]); //Sempre que la variable es sigueixi modificant, s'actualitzara el component useSTATE <!DOCTYPE html>
-  //pokemons es la variable i setPokemons la funció que els introdueix
+  const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     const fetchPokemons = async () => {
       try {
         const response = await getAllPokemons();
         setPokemons(response.data.results);
-        console.log(response.data.results);
       } catch (error) {
         console.error("Error fetching Pokémon list:", error);
         setPokemons([]);
@@ -20,15 +22,60 @@ function Pokesearch() {
     fetchPokemons();
   }, []);
 
+  useEffect(() => {
+    // Implement debouncing logic here
+    const timeoutId = setTimeout(() => {
+      if (searchTerm.trim() !== "") {
+        setIsSearching(true);
+        // Simulate server call here
+        const filteredPokemons = pokemons.filter(pokemon =>
+          pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filteredPokemons);
+        setIsSearching(false);
+      } else {
+        setSearchResults([]);
+      }
+    }, 500); // Adjust the debounce delay as needed
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [searchTerm, pokemons]);
+
   return (
     <div className="flex items-center justify-center py-4">
-      <input
-        type="text"
-        className="mr-3 px-4 py-2 rounded-lg border border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500"
-        placeholder="Buscar Pokémon..."
-      />
-      <div className="bg-red-500 text-white rounded-full p-2">
-        <SearchIcon />
+      <div className="relative">
+        <TextField
+          label="Buscar Pokémon..."
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <div className="bg-red-500 text-white rounded-full p-2 absolute right-2 top-2">
+                <SearchIcon />
+              </div>
+            ),
+            className:
+              "rounded-l-full rounded-r-none border-r-0 focus:ring-0 focus:border-gray-300",
+          }}
+          InputLabelProps={{
+            className: "bg-white px-2",
+          }}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        {searchResults.length > 0 && searchTerm.trim() !== "" && (
+          <div className="mt-1 bg-white rounded-md border border-gray-300 shadow-lg absolute w-full">
+            {isSearching ? (
+              <div className="p-2 text-center">Buscando...</div>
+            ) : (
+              searchResults.map((pokemon) => (
+                <div key={pokemon.name} className="p-2 border-b border-gray-300">
+                  {pokemon.name}
+                </div>
+              ))
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
